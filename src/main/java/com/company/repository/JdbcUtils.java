e6db9a6763154574870e6625e12e155653ba51ba
Darius Grigore Stoica <dariusgrigorestoica@gmail.com>
2025-08-17 09:48:25 +0300

    Replace old project with new Gradle project

--- src/main/java/com/company/repository/JdbcUtils.java
+++ src/main/java/com/company/repository/JdbcUtils.java
@@ -9,38 +9,27 @@ public class JdbcUtils {
     private final Properties jdbcProps;
 
 
-    public JdbcUtils(Properties props){
-        this.jdbcProps=props;
+    public JdbcUtils(Properties props) {
+        this.jdbcProps = props;
     }
 
     private Connection instance = null;
 
-    private Connection getNewConnection(){
-        String url=jdbcProps.getProperty("jdbc.url");
-        String user=jdbcProps.getProperty("jdbc.user");
-        String pass=jdbcProps.getProperty("jdbc.pass");
+    private Connection getNewConnection() {
+        String url = jdbcProps.getProperty("jdbc.url");
+        String user = jdbcProps.getProperty("jdbc.user");
+        String pass = jdbcProps.getProperty("jdbc.pass");
         System.out.println("trying to connect to database url = " + url);
-        Connection con=null;
+        Connection con = null;
         try {
 
-            if (user!=null && pass!=null)
-                con= DriverManager.getConnection(url,user,pass);
+            if (user != null && pass != null)
+                con = DriverManager.getConnection(url, user, pass);
             else
-                con=DriverManager.getConnection(url);
+                con = DriverManager.getConnection(url);
         } catch (SQLException e) {
-            System.out.println("Error getting connection "+e);
+            System.out.println("Error getting connection " + e);
         }
         return con;
-    }
-
-    public Connection getConnection(){
-        try {
-            if (instance==null || instance.isClosed())
-                instance=getNewConnection();
-
-        } catch (SQLException e) {
-            System.out.println("Error DB "+e);
-        }
-        return instance;
     }
 }
